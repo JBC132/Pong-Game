3ae3f60fadb582498d78734946f2f2d4fef93203
이진영 <jakerhee321@gmail.com>
2022-08-19 12:44:04 +0900

    Paddle and Ball Collision

--- Pong.py
+++ Pong.py
@@ -28,8 +28,8 @@ ball.shape("square")
 ball.color("white")
 ball.penup()
 ball.goto(0,0)
-ball.dx = 0.1
-ball.dy = 0.1
+ball.dx = 0.08
+ball.dy = 0.08
 
 def paddle_a_up():
     y = paddle_a.ycor()
@@ -79,4 +79,12 @@ while True:
     
     if ball.xcor() < -390:
         ball.goto(0,0)
+        ball.dx *= -1
+
+    if (ball.xcor() > 340 and ball.xcor() < 350) and (ball.ycor() < paddle_b.ycor() + 40 and ball.ycor() > paddle_b.ycor() -40):
+        ball.setx(340)
+        ball.dx *= -1
+    
+    if (ball.xcor() < -340 and ball.xcor() > -350) and (ball.ycor() < paddle_a.ycor() + 40 and ball.ycor() > paddle_a.ycor() -40):
+        ball.setx(-340)
         ball.dx *= -1
